c3a18f55e30f5e62c0c28c16c7d34629eb9b710e
Francis Maina <sebastianfrancix@gmail.com>
2022-09-14 00:24:31 +0300

    renders the sum of cards on the page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Create two variables, firstCard and secondCard and set their values to a random number between 2-11
 
-let firstCard = 11;
+let firstCard = 20;
 let secondCard = 1;
 
 let sum = firstCard + secondCard;
@@ -10,16 +10,25 @@ let isAlive = true;
 
 let message = "";
 
-//BlackJack logic
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Congratulations! You've got BlackJack";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
-}
+//Store the message paragraph in a variable called messageEl
+let messageEl = document.getElementById("message");
 
-console.log(sum);
-console.log(message);
+//store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el");
+
+//BlackJack logic
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Congratulations! You've got BlackJack";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+
+  //   render the sum on the page
+  sumEl.textContent = `Sum: ${sum}`;
+}
